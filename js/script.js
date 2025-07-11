31ba04f2af9af149a098f11fd40ad24abfc7abb1
Steopa2001 <moraru495@gmail.com>
2025-07-11 21:33:37 +0200

    feat: retrieving items from the dom

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//recupero elementi dal DOM
+const eleCount = document.getElementById('countdown');
+const startButton = document.getElementById('buttonStart');
+
+document.getElementById('instruction').innerText = 'inserisci i numeri che ricordi';
+
+
+
