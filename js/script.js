237f3e095c5b39c91030b4f0831c0ff590c0d2fc
Steopa2001 <moraru495@gmail.com>
2025-07-11 22:39:06 +0200

    feat: complerte timer

--- js/script.js
+++ js/script.js
@@ -3,20 +3,33 @@ const eleCount = document.getElementById('countdown');
 
 const randomNumbers = document.getElementById('numbers-list');
 
-document.getElementById('instructions').innerText = 'inserisci i numeri che ricordi';
+const istruzioni = document.getElementById('instructions');
 
 document.querySelectorAll('#answers-form')[0];
 // console.log(document.querySelectorAll('#answers-form')[0]);
 
 const arrayNumbers = [];
 
-const timer = 10;
-
+//creato numeri casuali
 for (let i = 0; i < 5; i++) {
    const numeroCasuale = Math.floor(Math.random() * 50) + 1;
    arrayNumbers.push(numeroCasuale);
-   randomNumbers.innerHTML += `<li>${numeroCasuale}</li>`
+   randomNumbers.innerHTML += `<li>${numeroCasuale}</li>`;
 }
+
+let count = 10;
+
+//creazione del timer 
+const timer = setInterval(() => {
+    if(count === 0) {
+        clearInterval(timer);
+     istruzioni.innerText = 'inserisci i numeri che ricordi'; 
+    } 
+    eleCount.innerHTML = count;
+    count--;
+}, 1000);
+
+
 
 
 
