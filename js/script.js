29ef1f758289e9e1c5076fb2af7fe6fdb6105259
Steopa2001 <moraru495@gmail.com>
2025-07-11 22:50:26 +0200

    feat: create time function

--- js/script.js
+++ js/script.js
@@ -1,39 +1,37 @@
 //recupero elementi dal DOM
-const eleCount = document.getElementById('countdown');
+const eleCount = document.getElementById("countdown");
 
-const randomNumbers = document.getElementById('numbers-list');
+const randomNumbers = document.getElementById("numbers-list");
 
-const istruzioni = document.getElementById('instructions');
+const istruzioni = document.getElementById("instructions");
 
-document.querySelectorAll('#answers-form')[0];
+const form = document.querySelectorAll("#answers-form");
+
 // console.log(document.querySelectorAll('#answers-form')[0]);
 
 const arrayNumbers = [];
 
 //creato numeri casuali
 for (let i = 0; i < 5; i++) {
-   const numeroCasuale = Math.floor(Math.random() * 50) + 1;
-   arrayNumbers.push(numeroCasuale);
-   randomNumbers.innerHTML += `<li>${numeroCasuale}</li>`;
+  const numeroCasuale = Math.floor(Math.random() * 50) + 1;
+  arrayNumbers.push(numeroCasuale);
+  randomNumbers.innerHTML += `<li>${numeroCasuale}</li>`;
 }
 
 let count = 10;
 
-//creazione del timer 
+//creazione del timer
 const timer = setInterval(() => {
-    if(count === 0) {
-        clearInterval(timer);
-     istruzioni.innerText = 'inserisci i numeri che ricordi'; 
-    } 
-    eleCount.innerHTML = count;
-    count--;
+  if (count === 0) {
+    //stoppato il timer appena arriva a 0
+    clearInterval(timer);
+    //cambio testo nel DOM 
+    istruzioni.innerText = "inserisci i numeri che ricordi";
+    //ho rimosso il display none del dorm nel DOM
+    form[0].classList.remove("d-none");
+    //ho rimosso la lista dei numeri casuali nel DOM
+    randomNumbers.classList.add("d-none");
+  }
+  eleCount.innerHTML = count;
+  count--;
 }, 1000);
-
-
-
-
-
-
-
-
-
