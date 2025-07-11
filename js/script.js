7d4f2a8766e5a66901af8a6cf3947b0a6c60f03c
Steopa2001 <moraru495@gmail.com>
2025-07-11 23:53:12 +0200

    end commit

--- js/script.js
+++ js/script.js
@@ -18,14 +18,14 @@ for (let i = 0; i < 5; i++) {
   randomNumbers.innerHTML += `<li>${numeroCasuale}</li>`;
 }
 
-let count = 10;
+let count = 9;
 
 //creazione del timer
 const timer = setInterval(() => {
   if (count === 0) {
     //stoppato il timer appena arriva a 0
     clearInterval(timer);
-    //cambio testo nel DOM 
+    //cambio testo nel DOM
     istruzioni.innerText = "inserisci i numeri che ricordi";
     //ho rimosso il display none del dorm nel DOM
     form[0].classList.remove("d-none");
@@ -35,3 +35,27 @@ const timer = setInterval(() => {
   eleCount.innerHTML = count;
   count--;
 }, 1000);
+
+const eleForm = form[0];
+function checkNumbers() {
+  let trueNumbers = "";
+  let countTrueNumbers = 0;
+
+  for (let i = 0; i < 5; i++) {
+    if (arrayNumbers.includes(parseInt(eleForm[i].value))) {
+      trueNumbers += eleForm[i].value + ', ';
+      countTrueNumbers++;
+    }
+  }
+
+  const eleMess = document.getElementById("message");
+  eleMess.innerText = `Il tuo score è: ${countTrueNumbers}/${arrayNumbers.length}. `;
+  if (countTrueNumbers > 0) {
+    eleMess.innerText += `(i numeri che hai indovinato sono: ${trueNumbers})`;
+  }
+}
+
+eleForm[5].addEventListener("click", (e) => {
+  e.preventDefault();
+  checkNumbers();
+});
