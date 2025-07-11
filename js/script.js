7af779f1b8ff1503f3f17c54f22bae93a0c05ea8
Steopa2001 <moraru495@gmail.com>
2025-07-11 22:12:20 +0200

    feat: complete number-list random for

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 //recupero elementi dal DOM
 const eleCount = document.getElementById('countdown');
-const startButton = document.getElementById('buttonStart');
 
-document.getElementById('instruction').innerText = 'inserisci i numeri che ricordi';
+const randomNumbers = document.getElementById('numbers-list');
+
+document.getElementById('instructions').innerText = 'inserisci i numeri che ricordi';
+
+document.querySelectorAll('#answers-form')[0];
+// console.log(document.querySelectorAll('#answers-form')[0]);
+
+const arrayNumbers = [];
+
+const timer = 10;
+
+for (let i = 0; i < 5; i++) {
+   const numeroCasuale = Math.floor(Math.random() * 50) + 1;
+   arrayNumbers.push(numeroCasuale);
+   randomNumbers.innerHTML += `<li>${numeroCasuale}</li>`
+}
+
+
+
+
 
 
 
