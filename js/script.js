fc70470213f90b71c165711b5a527cb369c8cc0a
Steopa2001 <moraru495@gmail.com>
2025-07-14 22:14:39 +0200

    correction

--- js/script.js
+++ js/script.js
@@ -1,61 +1,94 @@
-//recupero elementi dal DOM
-const eleCount = document.getElementById("countdown");
+//recupero gli elementi del dom che mi servono
+const numberList = document.getElementById("numbers-list");
+const form = document.getElementById("answers-form");
+const inputs = document.querySelectorAll("input");
+const countdownElement = document.getElementById("countdown");
+const message = document.getElementById("message");
+const instructions = document.getElementById("instructions");
 
-const randomNumbers = document.getElementById("numbers-list");
+//recupero bottone
+const button = document.querySelector("button");
 
-const istruzioni = document.getElementById("instructions");
+//definizione della funzione che genera i numeri random
+const generateRandomNumbers = (min, max) => {
+  //definisco un array vuoto in cui mettere i numeri generati
+  const random = [];
 
-const form = document.querySelectorAll("#answers-form");
+  //ciclo fino a che l'array random non abbia lunghezza 5
+  while (random.length < 5) {
+    //genero il numero casuale
+    const number = Math.floor(Math.random() * (max - min + 1)) + min;
 
-// console.log(document.querySelectorAll('#answers-form')[0]);
-
-const arrayNumbers = [];
-
-//creato numeri casuali
-for (let i = 0; i < 5; i++) {
-  const numeroCasuale = Math.floor(Math.random() * 50) + 1;
-  arrayNumbers.push(numeroCasuale);
-  randomNumbers.innerHTML += `<li>${numeroCasuale}</li>`;
-}
-
-let count = 9;
-
-//creazione del timer
-const timer = setInterval(() => {
-  if (count === 0) {
-    //stoppato il timer appena arriva a 0
-    clearInterval(timer);
-    //cambio testo nel DOM
-    istruzioni.innerText = "inserisci i numeri che ricordi";
-    //ho rimosso il display none del dorm nel DOM
-    form[0].classList.remove("d-none");
-    //ho rimosso la lista dei numeri casuali nel DOM
-    randomNumbers.classList.add("d-none");
-  }
-  eleCount.innerHTML = count;
-  count--;
-}, 1000);
-
-const eleForm = form[0];
-function checkNumbers() {
-  let trueNumbers = "";
-  let countTrueNumbers = 0;
-
-  for (let i = 0; i < 5; i++) {
-    if (arrayNumbers.includes(parseInt(eleForm[i].value))) {
-      trueNumbers += eleForm[i].value + ', ';
-      countTrueNumbers++;
+    //obiettivo: evito numeri duplicati nell'array random.
+    if (random.includes(number) === false) {
+      //inserisco il numero generato nell'array.
+      random.push(number);
     }
   }
 
-  const eleMess = document.getElementById("message");
-  eleMess.innerText = `Il tuo score è: ${countTrueNumbers}/${arrayNumbers.length}. `;
-  if (countTrueNumbers > 0) {
-    eleMess.innerText += `(i numeri che hai indovinato sono: ${trueNumbers})`;
-  }
+  return random;
+};
+
+//genero i numeri randomici
+const numbers = generateRandomNumbers(1, 50);
+
+//definisco la variabile che contiene i secondi
+let time = 10;
+
+//mostro i numeri a video
+//ciclo l'array numbers
+for (let i = 0; i < numbers.length; i++) {
+  numberList.innerHTML += `<li>${numbers[i]}</li>`;
 }
 
-eleForm[5].addEventListener("click", (e) => {
-  e.preventDefault();
-  checkNumbers();
+//mostro il valore time dentro l'elemento countdown
+countdownElement.innerText = time;
+
+//definisco il setInterval che mi scala i secondi e mi nasconde i numeri per farmi apparire la form
+const countdown = setInterval(() => {
+  //decremento valore di time
+  time--;
+  countdownElement.innerText = time;
+
+  //annullo setInterval quando time arriva a 0, mostro i campi input, nascondo i numeri e cancello intervallo.
+  if (time === 0) {
+    clearInterval(countdown);
+
+    //rimuovo la classe d-none a form.
+    form.classList.remove("d-none");
+
+    //aggiungo la classe d-none a numbers-list.
+    numberList.classList.add("d-none");
+    instructions.innerText = `Inserisci i numeri che ricordi e visualizza il risultato`;
+  }
+}, 1000);
+
+//definisco la funzione che al click del pulsante mi recupera i numeri che l'utente ha inserito e controlla quali sono presenti nell'array di quelli generati casualmente.
+const confirm = () => {
+  //definisco la variabile che mi contiene i numeri inseriti dall'utente
+  const userNumbers = [];
+
+  //definisco un nuovo array, in cui vado ad inserire solo i numeri che l'utente ha indovinato
+  const correctAnswers = [];
+
+  //ciclo inputs per recuperare i valori inseriti dall'utente e metterli nell'Array userNumbers.
+  for (let i = 0; i < inputs.length; i++) {
+    //inserisco il valore (trasformato in numero) all'interno dell'array userNumbers
+    userNumbers.push(parseInt(inputs[i].value));
+  }
+
+  //ciclo l'array userNumbers e confronto l'elemento attualmente ciclato con quelli presenti nell'array dei numeri generati casualmente.
+  for (let i = 0; i < userNumbers.length; i++) {
+    if (numbers.includes(userNumbers[i]) === true) {
+      correctAnswers.push(userNumbers[i]);
+    }
+  }
+
+  //mostriamo il messaggio all'utente.
+  message.innerText = `Hai indovinato ${correctAnswers.length} (${correctAnswers})`;
+};
+
+button.addEventListener("click", (e) => {
+  e.preventDefault;
+  confirm();
 });
